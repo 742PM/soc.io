efb4720803f740ed2d1667bdd7b59c0adad38d32
A.Voi <voytsishevskiy@tochka.com>
2020-10-04 16:06:10 +0500

    init project;

---
+++ src/main/kotlin/com/seven42/pm/soc/io/Application.kt
@@ -0,0 +1,11 @@
+package com.seven42.pm.soc.io
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/com/seven42/pm/soc/io/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seven42.pm.soc.io
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
